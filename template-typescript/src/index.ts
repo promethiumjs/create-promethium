9c422dace966df5760d61b737e64fac04699fd96
paglobal <jackamoah.pa@gmail.com>
2024-06-14 15:45:30 +0000

    update to match changes in promethium-js

--- template-typescript/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { h, renderTemplateFn } from "promethium-js";
-import { html } from "lit";
-import App from "./App";
-import "./index.css";
-
-renderTemplateFn(() => html`${h(App)}`, { renderContainer: "body" });
--- template-typescript/src/index.tsx
+++ template-typescript/src/index.tsx
@@ -1,6 +1,5 @@
-import { h } from "promethium-js";
 import { render } from "lit";
 import App from "./App";
 import "./index.css";
 
-render(h(App), document.body);
+render(<App />, document.body);
